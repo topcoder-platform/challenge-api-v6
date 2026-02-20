629b0b28d20f3245f828b8ee1fc9ea9ac70c6ffc
himaniraghav3 <himaniraghav0311@gmail.com>
2026-02-20 16:10:03 +0530

    Keep the invalid topic disabled

--- app-constants.js
+++ app-constants.js
@@ -96,6 +96,7 @@ const DisabledTopics = [
   Topics.ChallengeTimelineTemplateCreated,
   Topics.ChallengeTimelineTemplateUpdated,
   Topics.ChallengeTimelineTemplateDeleted,
+  Topics.ChallengePhaseUpdated,
   Topics.ChallengePhaseDeleted,
   Topics.DefaultChallengeReviewerCreated,
   Topics.DefaultChallengeReviewerUpdated,
