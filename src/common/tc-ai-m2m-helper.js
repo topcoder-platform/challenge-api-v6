23d2cb49508fc80895ee171f85270335d4d0c91a
Hentry Martin <hentrymartin.p@gmail.com>
2026-03-17 21:55:09 +0100

    deploy to dev

--- src/common/tc-ai-m2m-helper.js
+++ src/common/tc-ai-m2m-helper.js
@@ -6,9 +6,15 @@ class TCAIM2MHelper {
   static m2m = null;
 
   constructor() {
-    TCAIM2MHelper.m2m = m2mAuth(
-      _.pick(config, ["AUTH0_URL", "AUTH0_AUDIENCE", "TC_AI_M2M_AUTH0_CLIENT_ID", "TC_AI_M2M_AUTH0_CLIENT_SECRET", "AUTH0_PROXY_SERVER_URL"])
-    );
+    TCAIM2MHelper.m2m = m2mAuth({
+      AUTH0_URL: config.TC_AI_M2M_AUTH0_URL,
+      AUTH0_AUDIENCE: config.TC_AI_M2M_AUTH0_AUDIENCE,
+      AUTH_DOMAIN: config.TC_AI_M2M_AUTH_DOMAIN,
+      TOKEN_CACHE_TIME: config.TOKEN_CACHE_TIME,
+      AUTH0_CLIENT_ID: config.TC_AI_M2M_AUTH0_CLIENT_ID,
+      AUTH0_CLIENT_SECRET: config.TC_AI_M2M_AUTH0_CLIENT_SECRET,
+      AUTH0_PROXY_SERVER_URL: config.TC_AI_M2M_AUTH_PROXY_SERVER_URL,
+    });
   }
   /**
    * Get TC AI M2M token.
