53344c1ab3ad1e47e2f3659816cc6bb97d02ecca
jmgasper <jmgasper@gmail.com>
2026-06-10 16:23:12 +1000

    Whitelist changes for M2M tokens

---
+++ test/unit/helper-whitelist.test.js
@@ -0,0 +1,25 @@
+require("../../app-bootstrap");
+
+const { expect } = require("chai");
+const helper = require("../../src/common/helper");
+
+describe("challenge whitelist helper", () => {
+  it("does not apply challenge user whitelist checks to full M2M callers", () => {
+    expect(helper.shouldApplyChallengeWhitelist({ isMachine: true })).to.equal(false);
+  });
+
+  it("does not apply challenge user whitelist checks to whitelist-only M2M callers", () => {
+    expect(helper.shouldApplyChallengeWhitelist({ bypassChallengeWhitelist: true })).to.equal(
+      false,
+    );
+  });
+
+  it("continues to apply challenge user whitelist checks to interactive callers", () => {
+    expect(
+      helper.shouldApplyChallengeWhitelist({
+        roles: ["administrator"],
+        userId: "blocked-user",
+      }),
+    ).to.equal(true);
+  });
+});
